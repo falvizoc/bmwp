b68fc514369d58c5bd96f3a87b1a1eb2d1c803d2
Francisco Alvizo <falvizo@bitmovil.mx>
2020-07-14 18:38:48 -0500

    Modificaciones en footer, redes sociales

--- resources/views/layout/partials/footer.blade.php
+++ resources/views/layout/partials/footer.blade.php
@@ -16,10 +16,10 @@
                         </div>
                         <div class="col-md-6 text-md-right">
                             <ul class="socials">
-                                <li><a href="" class="icon-facebook fs-20"></a></li>
-                                <li><a href="" class="icon-instagram fs-20"></a></li>
-                                <li><a href="" class="icon-twitter fs-20"></a></li>
-                                <li><a href="" class="icon-linkedin fs-20"></a></li>
+                                <li><a href="{{url('https://bitmovil.mx')}}" target="_blank" class="icon-facebook fs-20"></a></li>
+                                <!-- <li><a href="" class="icon-instagram fs-20"></a></li>
+                                <li><a href="" class="icon-twitter fs-20"></a></li> -->
+                                <li><a href="{{url('https://linkedin.com/company/bitmovil')}}" target="_blank" class="icon-linkedin fs-20"></a></li>
                             </ul>
                         </div>
                     </div>
