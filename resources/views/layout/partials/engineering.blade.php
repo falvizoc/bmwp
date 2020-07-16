3cb5d1656b20c3e9bbfb4d72f79b3dd7d8e1e01e
Francisco Alvizo <falvizo@bitmovil.mx>
2020-07-15 22:59:55 -0500

    Integración parcial de Ingeniería como propuesta de valor

---
+++ resources/views/layout/partials/engineering.blade.php
@@ -0,0 +1,61 @@
+<!-- presentation -->
+<section class="section-lg pt-0">
+    <div class="container-fluid">
+        <div class="row my-10">
+            <div class="col-lg-6">
+                <div class="row">
+                    <div class="col">
+                        <h1 ><b class="text-danger">Ingeniería</b> <br>nuestra fuente generadora <br><b class="text-danger">valor.</b></h1>
+                    </div>
+                </div>
+
+                <div class="row gutter-3">
+                    <div class="col-md-6" data-aos="fade-up">
+                        <div class="media">
+                            <i class="icon-check-circle2 fs-30 text-twitter mr-2"></i>
+                            <div class="media-body">
+                                <h5 class="mt-0 text-uppercase font-weight-bold fs-14 letter-spacing">Enfoque estrategico</h5>
+                                <p>Desarrollamos proyectos que sostienen las operaciones centrales de nuestros clientes.</p>
+                            </div>
+                        </div>
+                    </div>
+
+                    <div class="col-md-6" data-aos="fade-up">
+                        <div class="media">
+                            <i class="icon-award fs-30 text-twitter mr-2"></i>
+                            <div class="media-body">
+                                <h5 class="mt-0 text-uppercase font-weight-bold fs-14 letter-spacing">Normas y estándares</h5>
+                                <p>Diseñamos soluciones bajo las normas y estándares más exigentes de la industria TI.</p>
+                            </div>
+                        </div>
+                    </div>
+
+                    <div class="col-md-6" data-aos="fade-up">
+                        <div class="media">
+                            <i class="icon-arrow-up-right fs-30 text-twitter mr-2"></i>
+                            <div class="media-body">
+                                <h5 class="mt-0 text-uppercase font-weight-bold fs-14 letter-spacing"> </h5>
+                                <p>Exploramos para identificar la necesidad real de nuestros clientes.</p>
+                            </div>
+                        </div>
+                    </div>
+
+
+                    <div class="col-md-6" data-aos="fade-up">
+                        <div class="media">
+                            <i class="icon-command fs-30 text-twitter mr-2"></i>
+                            <div class="media-body">
+                                <h5 class="mt-0 text-uppercase font-weight-bold fs-14 letter-spacing">Generación de valor</h5>
+                                <p>Enfocados en entregar el máximo rendimiento de los recursos de TI.</p>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+            <div class="col-12 col-lg-6 presentation presentation-responsive">
+                <img class="left-0 vertical-align" src="{{asset('assets/img/ingenieria.jpg')}}" style="height: 700px; width: auto; " alt="Image">
+            </div>
+        </div>
+    </div>
+</section>
+<!-- / presentation -->
