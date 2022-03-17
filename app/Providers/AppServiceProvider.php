c373af6f61824dbae9f4461386baf34daa723957
Francisco Alvizo <falvizo@me.com>
2022-03-16 23:41:49 -0600

    Actualización a laravel 8

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -3,6 +3,7 @@
 namespace App\Providers;
 
 use GuzzleHttp\Client;
+use Illuminate\Pagination\Paginator;
 use Illuminate\Support\ServiceProvider;
 
 class AppServiceProvider extends ServiceProvider
@@ -31,6 +32,6 @@ class AppServiceProvider extends ServiceProvider
      */
     public function boot()
     {
-
+        Paginator::useBootstrap();
     }
 }
