90f10e8f357aea6f4e8b15b7fe5a399d9e6de962
Francisco Alvizo <falvizo@bitmovil.mx>
2022-05-04 23:12:07 -0500

    Activación Bootstrap

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -33,5 +33,12 @@ class AppServiceProvider extends ServiceProvider
     public function boot()
     {
         Paginator::useBootstrap();
+        if($this->app->environment('production')) {
+            \URL::forceScheme('https');
+        }
     }
+
+
+
+
 }
