cf7a0caff9619506d45d9b6781ded8217c51c883
Francisco Alvizo <falvizo@bitmovil.mx>
2024-03-26 15:57:38 -0600

    Cambios en el menú

--- resources/views/layout/partials/nav.blade.php
+++ resources/views/layout/partials/nav.blade.php
@@ -8,66 +8,28 @@
         </button>
     <div class="collapse navbar-collapse" id="navbarSupportedContent">
         <ul class="navbar-nav mr-auto left">
+            <!-- Tienda en línea
             <li class="nav-item">
                 <a href="{{ url('https://store.bitmovil.mx') }}" target="_blank" class="badge badge-pill badge-red " ><small class="text-white"> <i class="icon-shopping-cart"></i> Tienda en línea</small></a>
             </li>
+-->
+            <!-- Nosotros -->
             <li class="nav-item dropdown-mega show">
-                <a href="" class="nav-link text-white dropdown-toggle" id="softwareMenu" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
-                    <span class="text-white">Soluciones</span>
+                <a href="" class="nav-link text-white" id="softwareMenu" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+                    <span class="text-white">Nosotros</span>
                 </a>
-                <div class="dropdown-menu container" aria-labelledby="navbarDropdown-1" style="top:45px;">
-                    <div class="row">
-                        <div class="col">
-                            <ul class="mega-list">
-                                <!-- Seguridad -->
-                                <li><span><ion-icon name="lock-closed-outline"></ion-icon> Seguridad</span></li>
-                                <div class="dropdown-divider"></div>
-                                <li><a class="text-dark" href="{{ url('/controldeacceso') }}">Control de acceso <p>Torniquetes | Chapas | Biométricos</p></a></li>
-                                <li><a class="text-dark" href="{{ url('/videovigilancia') }}">Videovigilancia <p>Cámaras | Video Wall | VMS </p></a></li>
-                            </ul>
-                        </div>
-                        <div class="col">
-                            <ul class="mega-list">
-                                <!-- Redes -->
-                                <li><span><ion-icon name="git-network-outline"></ion-icon> Redes</span></li>
-                                <div class="dropdown-divider"></div>
-                                <li><a class="text-dark" href="{{ url('/redesinalambricas') }}">Redes inalámbricas<p> Redes unificadas | Enlaces</p></a></li>
-                                <li><a class="text-dark" href="{{ url('/') }}">Networking<p>Switches | Routers | Firewalls</p></a></li>
-                            </ul>
-                        </div>
-                        <div class="col">
-                            <ul class="mega-list">
-                                <!-- Energía -->
-                                <li><span><ion-icon name="flash-outline"></ion-icon> Energía </span></li>
-                                <div class="dropdown-divider"></div>
-                                <li><a class="text-dark" href="{{ url('/') }}">Sistemas de respaldo<p>UPS On Line | Bancos de baterías</p></a></li>
-                                <li><a class="text-dark" href="{{ url('/') }}">Plantas automatizadas <p>Tableros de transferencia</p></a></li>
-                            </ul>
-                        </div>
-                        <!--
-                        <div class="col">
-                            <ul class="mega-list">
-                                 Fuego
-                                <li><span><ion-icon name="flame-outline"></ion-icon> Detección de Fuego </span></li>
-                                <div class="dropdown-divider"></div>
-                                <li><a class="text-dark" href="{{ url('/deteciondefuego') }}">Honeywell<p>Páneles | Sistemas de alarma</p></a></li>
-                                <li><a class="text-dark" href="{{ url('/') }}">Plantas automatizadas <p>Tableros de transferencia</p></a></li
-                            </ul>
-                        </div>
-                        -->
-                    </div>
-                </div>
             </li>
+            <!-- Portafolio -->
             <li class="nav-item dropdown-mega show">
                 <a href="" class="nav-link text-white dropdown-toggle" id="softwareMenu" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
-                    <span class="text-white">Nuestro Portafolio</span>
+                    <span class="text-white">Portafolio</span>
                 </a>
                 <div class="dropdown-menu container" aria-labelledby="navbarDropdown-1" style="top:45px;">
                     <div class="row">
                         <div class="col">
                             <ul class="mega-list">
                                 <!-- Cableado Estructurado -->
-                                <li><span><ion-icon name="lock-closed-outline"></ion-icon> Infraestructura Física de Redes</span></li>
+                                <li><span><ion-icon name="git-network-outline"></ion-icon> Infraestructura Física de Redes</span></li>
                                 <div class="dropdown-divider"></div>
                                 <li><a class="text-dark" href="{{ url('/controldeacceso') }}">Control de acceso <p>Torniquetes | Chapas | Biométricos</p></a></li>
                                 <li><a class="text-dark" href="{{ url('/videovigilancia') }}">Videovigilancia <p>Cámaras | Video Wall | VMS </p></a></li>
@@ -124,77 +86,28 @@
                 </div>
             </li>
 
+            <!-- Partners y certificaciones -->
             <li class="nav-item dropdown show">
-                <a href="" class="nav-link dropdown-toggle text-white" id="navbarDropdown" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="true">
-                    <span class="text-white">Software</span>
+                <a href="" class="nav-link text-white" id="navbarDropdown" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="true">
+                    <span class="text-white">Certificaciones</span>
                 </a>
-                <div class="dropdown-menu " aria-labelledby="navbarDropdown">
-                   <!-- <a href="{{ url('/microsoft365') }}" class="dropdown-item">-->
-                    <a href="{{ url('/#') }}" class="dropdown-item">
-                        <span><ion-icon name="logo-windows"></ion-icon> Microsoft</span>
-                        <p>Licenciamiento para Empresas, Escuelas y Hogares</p>
-                    </a>
-                    <div class="dropdown-divider"></div>
-                    <a href="{{ url('/') }}" class="dropdown-item">
-                        <span><ion-icon name="bug"></ion-icon> Antivirus</span>
-                        <p>Contra Virus o Malware</p>
-                    </a>
-                </div>
             </li>
-            <li class="nav-item dropdown show">
-                <a href="" class="nav-link dropdown-toggle text-white" id="navbarDropdown" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="true">
-                    <span class="text-white">Infraestructura</span>
-                </a>
-                <div class="dropdown-menu " aria-labelledby="navbarDropdown">
-                    <a href="{{ url('/') }}" class="dropdown-item">
-                        <span><ion-icon name="hardware-chip-outline"></ion-icon>  Servidores y cómputo</span>
-                        <p>HP | Lenovo | Dell</p>
-                    </a>
-                    <div class="dropdown-divider"></div>
-                    <a href="{{ url('/') }}" class="dropdown-item">
-                        <span><ion-icon name="server-outline"></ion-icon> Almacenamiento</span>
-                        <p>Synology</p>
-                    </a>
-                    <div class="dropdown-divider"></div>
-                    <a href="{{ url('/cableadoestructurado') }}" class="dropdown-item">
-                        <span><ion-icon name="analytics-outline"></ion-icon> Cableado Estructurado</span>
-                        <p>Synology</p>
-                    </a>
-                    <div class="dropdown-divider"></div>
-                    <a href="{{ url('/') }}" class="dropdown-item">
-                        <span><ion-icon name="locate-outline"></ion-icon> Fibra óptica</span>
-                        <p>Synology</p>
-                    </a>
-                </div>
-            </li>
-            <li class="nav-item dropdown show">
-                <a href="" class="nav-link dropdown-toggle text-white" id="navbarDropdown" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="true">
-                    <span class="text-white">Servicios</span>
-                </a>
-                <div class="dropdown-menu " aria-labelledby="navbarDropdown">
-                    <a href="{{ url('/') }}" class="dropdown-item">
-                        <span><ion-icon name="build-outline"></ion-icon>  Pólizas de soporte</span>
-                        <p>Aumente la disponibilidad de su infraestrucutra de TI</p>
-                    </a>
-                    <div class="dropdown-divider"></div>
-                    <a href="{{ url('/') }}" class="dropdown-item">
-                        <span><ion-icon name="people-outline"></ion-icon> Monitoristas</span>
-                        <p>Personal para Videovigilancia 24/7</p>
-                    </a>
-                    <div class="dropdown-divider"></div>
-                    <a href="{{ url('/') }}" class="dropdown-item">
-                        <span><ion-icon name="cash-outline"></ion-icon> Servicios Financieros</span>
-                        <p>Leasing para Activos de TI</p>
-                    </a>
-                    <div class="dropdown-divider"></div>
-                    <a href="{{ url('/') }}" class="dropdown-item">
-                        <span><ion-icon name="code-slash-outline"></ion-icon> Desarrollo Web</span>
-                        <p>Sitios y Aplicaciones Web</p>
-                    </a>
 
-                </div>
+            <!-- Casos de éxito -->
+            <li class="nav-item dropdown show">
+                <a href="" class="nav-link text-white" id="navbarDropdown" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="true">
+                    <span class="text-white">Casos de éxito</span>
+                </a>
             </li>
-            <li class="nav-item"><a href="{{ url('/foros') }}" class="nav-link text-white"><span class="text-white">Foros</span></a></li>
+
+            <!-- Blog y recursos-->
+            <li class="nav-item dropdown show">
+                <a href="" class="nav-link text-white" id="navbarDropdown" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="true">
+                    <span class="text-white">Blog y Recursos</span>
+                </a>
+            </li>
+
+            <!-- Contacto -->
             <li class="nav-item"><a href="{{ url('https://wa.link/bitmovil') }}" target="_blank"  class="nav-link text-white"><span class="text-white">Contacto</span></a></li>
         </ul>
         <ul class="navbar-nav mr-1">
